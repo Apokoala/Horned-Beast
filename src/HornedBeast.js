4bda80c35abaf9b975f8b4535704fbde13a582a8
Apokoala <michael_gazaway@hotmail.com>
2022-10-02 20:34:25 -0400

    labtwo

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
+  //anytime you have state inside an object you have to have a constructor to instantiate the props.
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: ""
+    }
+  }
+
+  handleClick = () => this.setState({ favorites: this.state.favorites + "💗" });
+
+
   render() {
     return (
-      <>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description} />
-        <p>{this.props.description}</p>
-      </>
-   )
+      <Col>
+        <Card onClick={this.handleClick} className = "h-100 p-2">
+          <h2>{this.props.title}</h2>
+          <img src={this.props.imageUrl} alt={this.props.description} />
+          <p>{this.props.description}</p>
+          <p>Favor: {this.state.favorites}</p>
+        </Card>
+      </Col>
+    )
   }
 }
 
